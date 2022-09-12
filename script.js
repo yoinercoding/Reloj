0fae64724b7ceec20d4ecbb92ede84ea02382212
ybello <yoinercoding@gmail.com>
2022-09-12 09:28:05 -0300

    Script, logica

---
+++ script.js
@@ -0,0 +1,41 @@
+const deg = 6
+const hour = document.querySelector(".hour")
+const min = document.querySelector(".min")
+const sec = document.querySelector(".sec")
+
+const setClock = () => {
+    let day = new Date()
+    let hh = day.getHours * 30
+    let mm = day.getMinutes() * deg
+    let ss = day.getSeconds() * deg
+
+    hour.style.transform = `rotateZ(${hh + mm / 12}deg)`
+    min.style.transform = `rotateZ(${mm}deg)`
+    sec.style.transform = `rotateZ(${ss}deg)`
+}
+
+setInterval(setClock, 1000)
+
+switchTheme = (evt) => {
+    const switchBtn = evt.target
+    if (switchBtn.textContent.toLowerCase() === 'light') 
+    {
+      switchBtn.textContent = 'dark'
+      localStorage.setItem('theme', 'dark')
+      document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+      switchBtn.textContent = 'light'
+      localStorage.setItem('theme', 'light')
+      document.documentElement.setAttribute('data-theme', 'light')
+    }
+  }
+  
+  const switchModeBtn = document.querySelector('.switch-btn')
+  switchModeBtn.addEventListener('click', switchTheme, false)
+  
+  const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null
+  
+  if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme)
+    switchModeBtn.textContent = currentTheme;
+  }
